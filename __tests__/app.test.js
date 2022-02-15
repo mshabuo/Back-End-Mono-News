432d54852c70d04a6d10e636ed2c9f1e9a35af31
“mshabuo” <moe.shabuo@hotmail.co.uk>
2022-02-15 13:17:22 +0000

    ncNews# completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -32,4 +32,28 @@ it('responds with 404 error if not path is found', ()=>{
     })
 });
   })
+  describe('/api/articles/:article_id', ()=>{
+    test('responds with an article object', ()=>{
+      return request(app).get('/api/articles/1').expect(200).then((response)=>{
+         expect(response.body.article).toHaveLength(1)
+        response.body.article.forEach((article) =>{
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              body: expect.any(String),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number)
+            }))
+        })
+      })
+    })
+    test('responds with 400 for bad requests', ()=>{
+      return request(app).get('/api/articles/adam').expect(400).then(({body: {msg}})=>{
+        expect(msg).toBe("bad request")
+      })
+    })
+  })
 })
