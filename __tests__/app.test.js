3c6d27b386c4365e68b8087bd1056a6ad0762ef9
“mshabuo” <moe.shabuo@hotmail.co.uk>
2022-02-27 20:56:21 +0000

    updates completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,12 +10,12 @@ beforeEach(()=> (seed(data)))
 
 
 describe('GET', ()=> {
-  // tests topics end-point - fetch topic
+  
         describe('/api/topics', ()=>{
             test('responds with array of topic objects', () => {
-            return request(app).get('/api/topics').expect(200).then((response) =>{
-              expect(response.body.data).toHaveLength(3)
-                response.body.data.forEach((topic) =>{
+            return request(app).get('/api/topics').expect(200).then((res) =>{
+              expect(res.body.data).toHaveLength(3)
+                res.body.data.forEach((topic) =>{
                   expect(topic).toEqual(
                     expect.objectContaining({
                     description: expect.any(String),
@@ -24,7 +24,7 @@ describe('GET', ()=> {
             })            
 })
 })
-// tests error response for invalid path
+
 test('responds with 404 error if not path is found', ()=>{
     return request(app).get('/api/no-valid-path').expect(404)
     .then(({body: {msg}}) => {
@@ -32,12 +32,12 @@ test('responds with 404 error if not path is found', ()=>{
     })
 });
   })
-// tests GET request for articles end-points, fetch article by article_id
+
   describe('/api/articles/:article_id', ()=>{
     test('responds with an article object of article id 1', ()=>{
-      return request(app).get('/api/articles/1').expect(200).then((response)=>{
-         expect(response.body.article).toHaveLength(1)
-        response.body.article.forEach((article) =>{
+      return request(app).get('/api/articles/1').expect(200).then((res)=>{
+         expect(res.body.article).toHaveLength(1)
+        res.body.article.forEach((article) =>{
           expect(article).toEqual(
             expect.objectContaining({
               author: expect.any(String),
@@ -50,23 +50,22 @@ test('responds with 404 error if not path is found', ()=>{
             }))
         })
       })
-    })
-    // tests error response for bad request
+    });
+  
     test('responds with 400 for bad requests', ()=>{
       return request(app).get('/api/articles/adam').expect(400).then(({body: {msg}})=>{
-        expect(msg).toBe("bad request")
+        expect(msg).toBe("Bad Request")
       })
     });
-    // tests error response when id is not valid
     test('responds with 404 when id currently does not exist in the database', () =>{
       return request(app).get('/api/articles/6000').expect(404).then(({body: {msg}})=> {
         expect(msg).toBe("article id not found")
       })
     })
   })
-})
+});
 
-// tests PATCH request for articles end-point, fetching articles by article_id
+
 describe('PATCH', () =>{
   describe('PATCH /api/articles/:article_id', ()=>{
     test('increment article#2 vote by 1 and responds with an updated article', () =>{
@@ -82,29 +81,29 @@ describe('PATCH', () =>{
     created_at: '2020-10-16T05:03:00.000Z',
     votes: 1
       }
-      return request(app).patch('/api/articles/2').send(incrementVote).expect(201).then((response) => {
-        expect(response.body.article).toEqual(testOutput)
+      return request(app).patch('/api/articles/2').send(incrementVote).expect(201).then((res) => {
+        expect(res.body.article).toEqual(testOutput)
         })
       }) 
-// testing articles with more than 0 votes can be incremented
+
        test('increment article#1 vote by 20 and responds with an updated article', () =>{
       const incrementVote = {
         votes: 20
       }
       const testOutput = {
     article_id: 1,
-          title: "Living in the shadow of a great man",
+    title: "Living in the shadow of a great man",
     topic: "mitch",
     author: "butter_bridge",
     body: "I find this existence challenging",
     created_at: '2020-07-09T20:11:00.000Z',
     votes: 120
       }
-      return request(app).patch('/api/articles/1').send(incrementVote).expect(201).then((response) => {
-        expect(response.body.article).toEqual(testOutput)
+      return request(app).patch('/api/articles/1').send(incrementVote).expect(201).then((res) => {
+        expect(res.body.article).toEqual(testOutput)
         })
       }) 
-// testing article votes can be decremented
+
       test('decrement article#1 by 10 and respond with updated article', ()=>{
         const decrementVote = {
           votes: -10
@@ -118,28 +117,28 @@ describe('PATCH', () =>{
     created_at: '2020-07-09T20:11:00.000Z',
     votes: 90
         }
-        return request(app).patch('/api/articles/1').send(decrementVote).expect(201).then((response)=>{
-          expect(response.body.article).toEqual(testOutput)
+        return request(app).patch('/api/articles/1').send(decrementVote).expect(201).then((res)=>{
+          expect(res.body.article).toEqual(testOutput)
         })
       })
-// testing value data type is valid input
+
       test('check all values are valid and responds with status 400 if invalid', ()=>{
         const invalidValue = {
           votes: 'not valid'
         }
         return request(app).patch('/api/articles/1').send(invalidValue).expect(400).then(({body: {msg}})=>{
-          expect(msg).toBe('bad request')
+          expect(msg).toBe('Bad Request')
         })
       })
     })
   })
 
-// tests GET request for users end-point to fetch users table
+
 describe('GET ', ()=>{
   describe('GET users table', ()=>{
     test('responds with an array of objects for all users in the table', ()=>{
-      return request(app).get('/api/users').expect(200).then((response)=>{
-        response.body.users.forEach((user)=>{
+      return request(app).get('/api/users').expect(200).then((res)=>{
+        res.body.users.forEach((user)=>{
           expect(user).toEqual(
             expect.objectContaining({
               username: expect.any(String),
@@ -150,7 +149,7 @@ describe('GET ', ()=>{
         })
       })
     })
-// tests error response where path provided is not valid
+
     test('responds with 404 error if path not found', ()=>{
     return request(app).get('/api/no-valid-path').expect(404)
     .then(({body: {msg}}) => {
@@ -160,8 +159,9 @@ describe('GET ', ()=>{
   })
 })
 
-// tests GET request for articles end-point, get comments by article_id
+
 describe("/api/:article_id/comments", () => {
+
   test("responds with status 200 and comments of the article id", () => {
     return request(app)
       .get("/api/articles/3/comments")
@@ -171,7 +171,7 @@ describe("/api/:article_id/comments", () => {
         expect(res.body).toHaveLength(2);
       });
   });
-  //tests error response where article_id doesn't exist
+  
   test("responds with 404 if no comment found", () => {
     return request(app)
     .get("/api/articles/60/comments")
@@ -180,4 +180,213 @@ describe("/api/:article_id/comments", () => {
       expect(msg).toBe('comments not found')
     })
   })
+});
+
+
+describe('GET/api/articles?query', () => {
+  test("responds with status 200 & and articles sorted by title in ascending order", () => {
+    return request(app)
+      .get("/api/articles?sort_by=title&&order=asc")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toBeSortedBy("title", {
+          ascending: true
+        });
+      });
+  });
+  test("responds with status 200 & articles sorted by author in descending order", () => {
+    return request(app)
+      .get("/api/articles?sort_by=author")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toBeSortedBy("author", {
+          descending: true
+        })
+      })
+  });
+test("respond with status 200 & and articles sorted by votes in descending order", () => {
+    return request(app)
+      .get("/api/articles?sort_by=votes&order=desc")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toBeInstanceOf(Array);
+        expect(res.body.articles).toBeSortedBy("votes", {
+          descending: true,
+        })
+      })
+    });
+test("respond with status 200 & and articles sorted by topic in descending order", () => {
+    return request(app)
+      .get("/api/articles?sort_by=topic&order=desc")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toBeInstanceOf(Array);
+        expect(res.body.articles).toBeSortedBy("topic", {
+          descending: true,
+        })
+      })
+    })
+    });
+
+test("respond with status 200 & and articles sorted by created_at in descending order", () => {
+    return request(app)
+      .get("/api/articles?sort_by=created_at&order=desc")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.articles).toBeInstanceOf(Array);
+        expect(res.body.articles).toBeSortedBy("created_at", {
+          descending: true,
+        })
+      })
+    });
+test("respond with status 400 when topic is not found in db", () => {
+    return request(app)
+      .get("/api/articles?sort_by=inavlid_topic")
+      .expect(400)
+      .then((res) => {
+      expect(res.body.msg).toBe("Invalid sort query");
+      })
+    });
+test("respond with status 400 when order is not permitted", () => {
+    return request(app)
+      .get("/api/articles?order=inavlid_order")
+      .expect(400)
+      .then((res) => {
+      expect(res.body.msg).toBe("Invalid order query");
+      })
+    });
+
+test('respond with status 400 if fields are missing', ()=>{
+const testOutput = {
+      author: "lurker",
+      body: "test body",
+      topic: "cats",
+    };
+     return request(app)
+      .post("/api/articles")
+      .send(testOutput)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+   })
+  });
+
+describe("/api/articles", () => {
+  test("POST status 201 and return new article", () => {
+    const newArticle = {
+      author: "butter_bridge",
+      title: "Test article by Lurker",
+      body: "test body article",
+      topic: "mitch",
+    };
+
+    const expectedArticle = {
+      article_id: 13,
+      title: 'Test article by Lurker',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'test body article',
+      created_at: expect.any(String),
+      votes: 0,
+    }
+    return request(app)
+      .post("/api/articles")
+      .send(newArticle)
+      .expect(201)
+      .then((res) => {
+        expect(res.body.article).toEqual(expectedArticle);
+      }) 
+  });
+
+  test("POST 400 status for missing title field", () => {
+    const newArticle = {
+      author: "lurker",
+      body: "This is a very exciting test article about cats.",
+      topic: "cats",
+    };
+
+    return request(app)
+      .post("/api/articles")
+      .send(newArticle)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+    })
+  
+  test("POST status 400 for invalid input type", () => {
+    const newArticle = {
+      author: "lurker",
+      title: "Test article by Lurker",
+      body: 1,
+      topic: "cats",
+    };
+
+    return request(app)
+      .post("/api/articles")
+      .send(newArticle)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+  });
+  test("POST status 422 for non-existent author", () => {
+    const newArticle = {
+      author: "invalid_author",
+      title: "Test article by Lurker",
+      body: "This is a very exciting test article about cats.",
+      topic: "cats",
+    };
+
+    return request(app)
+      .post("/api/articles")
+      .send(newArticle)
+      .expect(422)
+      .then((res) => {
+        expect(res.body.msg).toBe("Unprocessable Entity");
+      });
+  });
+  test("POST status 400 for invalid author input", () => {
+    const newArticle = {
+      author: 8,
+      title: "Test article by Lurker",
+      body: "This is a very exciting test article about cats.",
+      topic: "cats",
+    };
+
+    return request(app)
+      .post("/api/articles")
+      .send(newArticle)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+  });
+  test("POST 422 status when topic doesn't exist", () => {
+    const newArticle = {
+      author: "lurker",
+      title: "Test article by Lurker",
+      body: "This is a very exciting test article about cats.",
+      topic: "doesnt_exist",
+    };
+
+    return request(app)
+      .post("/api/articles")
+      .send(newArticle)
+      .expect(422)
+      .then((res) => {
+        expect(res.body.msg).toBe("Unprocessable Entity");
+      });
+  });
+})
+
+describe("/api/comments/:comment_id", () => {
+  test("DELETE: status 400 & returns an error message", () => {
+    return request(app)
+      .delete("/api/comments/invalid_id")
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+  });
 });
