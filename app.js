77ba56eb8b50c2d74157b176068e201bea9e6a4b
“mshabuo” <moe.shabuo@hotmail.co.uk>
2022-03-07 12:00:48 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const cors = require ('cors');
 const express = require ('express');
 const app = express();
+app.use(cors());
 const {getArticleById, patchArticle, queryArticle, postArticle} = require("./controllers/articles.controller")
 const {deleteComment, postCommentByArticleId, fetchCommentsByArticleId} = require("./controllers/comments.controller")
 const {fetchTopic} = require("./controllers/topics.controller")
